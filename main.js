561902d45e17306e27e9751a564db7a9578d9afb
Cargalher <cargalher@gmail.com>
2021-04-27 16:58:23 +0200

    applied conditional statements for multiplo 3, 5 and 3&&5

--- main.js
+++ main.js
@@ -8,8 +8,35 @@
 var numbers = [];
 
 for (var i=1; i<=100; i++) {
-    console.log(i);
+    // console.log(i);
   numbers.push([i]);
-  }
-  console.log(numbers);
-  
+}
+    //console.log(numbers);
+
+
+
+for (var j=0; j<numbers.length; j++){
+
+    if(numbers[j] % 3 === 0 && numbers[j] % 5 === 0){
+
+        console.log("FizzBuzz");
+        document.getElementById("FizzBuzz").innerHTML="FizzBuzz";
+
+    } else if (numbers[j] % 5 === 0) {
+
+        console.log("Buzz");
+        document.getElementById("FizzBuzz").innerHTML="Buzz";
+
+    } else if(numbers[j] % 3 === 0){
+
+        console.log("Fizz");
+        document.getElementById("FizzBuzz").innerHTML="Fizz";
+
+    } else {
+
+        console.log(numbers[j]);
+        document.getElementById("FizzBuzz").innerHTML=numbers[j];
+
+    }
+
+}
